ad50e128319cdc24dac73182ccc9784fc7eebd18
initialencounter <2911583893@qq.com>
2025-04-10 23:33:31 +0800

    fix desktop
    Signed-off-by: initialencounter <2911583893@qq.com>

--- pkgs/sunloginclient/default.nix
+++ pkgs/sunloginclient/default.nix
@@ -5,6 +5,7 @@
 , lib
 , makeWrapper
 , xorg
+, gcc
 , glibc
 , libgcc
 , libuuid
@@ -12,28 +13,29 @@
 , libappindicator-gtk3
 , systemd
 , ncurses5
-, libxcrypt
+, libxcrypt-legacy
 }:
 
 let
   # 创建兼容层
+  # https://github.com/luisholanda/dotfiles/blob/dc947fe00d89c089617f779fd2aa6f72f68904b7/overlays/applications/games/steam.nix#L17
   libcrypt-compat = stdenv.mkDerivation {
     name = "libcrypt-compat";
-    buildInputs = [ libxcrypt ];
+    buildInputs = [ libxcrypt-legacy ];
     phases = [ "installPhase" ];
     installPhase = ''
       mkdir -p $out/lib
-      ln -s ${libxcrypt}/lib/libcrypt.so $out/lib/libcrypt.so.1
+      ln -s ${libxcrypt-legacy}/lib/libcrypt.so $out/lib/libcrypt.so.1
     '';
   };
 in
 
 stdenv.mkDerivation rec {
   pname = "sunloginclient-${version}";
-  version = "10.1.1.28779";
+  version = "15.2.0.63064";
   src = fetchurl {
-    url = "https://dw.oray.com/sunlogin/linux/sunloginclientshell-${version}-amd64.deb";
-    sha256 = "sha256-kjqL/bEZTuUNjG3XtRoPFqs0s0FMdo9f+itJ2Ah07tU=";
+    url = "https://dw.oray.com/sunlogin/linux/SunloginClient_${version}_amd64.deb";
+    sha256 = "sha256-3a5dNk64tpQGoOGDSQRQ/S+R8HKXKzcI6VGOiFLLimM=";
   };
 
   nativeBuildInputs = [
@@ -52,6 +54,7 @@ stdenv.mkDerivation rec {
     xorg.libXi
     xorg.libXrender
     xorg.libXau
+    gcc.cc.lib
     glibc
     libgcc
     libuuid
@@ -75,6 +78,20 @@ stdenv.mkDerivation rec {
     ln -s $out/usr/local/sunlogin/bin/sunloginclient $out/bin/sunloginclient
     ln -s $out/usr/local/sunlogin/bin/oray_rundaemon $out/bin/oray_rundaemon
     
+    # 安装 .desktop 文件
+    mkdir -p $out/share/applications
+    cp usr/share/applications/sunlogin.desktop $out/share/applications/
+
+    # 修复 .desktop 文件中的路径
+    substituteInPlace $out/share/applications/sunlogin.desktop \
+      --replace "/usr/local/sunlogin" "$out/usr/local/sunlogin"
+    
+    # 安装图标（如果有）
+    if [ -d "usr/share/icons" ]; then
+      mkdir -p $out/share/icons
+      cp -r usr/share/icons/* $out/share/icons/
+    fi
+
     # 创建systemd服务文件
     mkdir -p $out/etc/systemd/system
     cat > $out/etc/systemd/system/runsunloginclient.service <<EOF
@@ -103,12 +120,13 @@ stdenv.mkDerivation rec {
       --replace "/usr/local/sunlogin" "$out/usr/local/sunlogin"
     
     chmod +x $out/usr/local/sunlogin/scripts/*.sh
+    # 确保 .desktop 文件可执行
+    chmod +x $out/share/applications/sunlogin.desktop
   '';
 
   meta = with lib; {
     description = "Sunlogin Remote Control Client";
     homepage = "https://sunlogin.oray.com/";
-    license = lib.licenses.unfree;
     platforms = [ "x86_64-linux" ];
   };
 }
