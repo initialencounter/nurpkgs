f0c618dd632048b8b859eb464ffec8de98194eaf
initialencounter <2911583893@qq.com>
2025-05-27 18:46:53 +0800

    fix(easytier-gui): pnpm build
    Signed-off-by: initialencounter <2911583893@qq.com>

--- pkgs/easytier-gui/unwrapped.nix
+++ pkgs/easytier-gui/unwrapped.nix
@@ -51,6 +51,9 @@ rustPlatform.buildRustPackage {
     substituteInPlace $cargoDepsCopy/libappindicator-sys-*/src/lib.rs \
       --replace-fail "libayatana-appindicator3.so.1" "${libayatana-appindicator}/lib/libayatana-appindicator3.so.1"
 
+    substituteInPlace easytier-gui/src-tauri/tauri.conf.json \
+      --replace-fail "pnpm build" "pnpm -r build"
+
     # this file tries to override the linker used when compiling for certain platforms
     rm .cargo/config.toml
 
